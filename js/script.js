5b7f37d38d61f014b19949ff93b5ddcd7e317162
taka <taka@users.noreply.github.com>
2024-11-18 13:31:59 +0900

    ハンバーガーボタンやメニューの作成、CSSアニメーション、JS実装。

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+// スワイパー設定。
 const swiper = new Swiper('.swiper', {
   loop: true,
 
@@ -16,7 +17,7 @@ const swiper = new Swiper('.swiper', {
 
 });
 
-
+// 少しでもスクロールするとヘッダーの高さを小さくする。
 document.addEventListener('scroll', function () {
   if (window.scrollY > 0) {
     document.querySelector('.l-header').classList.add('l-header--hasScrolled');
@@ -24,3 +25,23 @@ document.addEventListener('scroll', function () {
     document.querySelector('.l-header').classList.remove('l-header--hasScrolled');
   }
 });
+
+// ------------------------------------------------------------------
+// ハンバーガーボタンやメニュークリック時のイベントハンドラ。
+// ------------------------------------------------------------------
+
+// ハンバーガーボタンクリックによるメニュー表示。
+document.querySelector('.l-header__humBtn').addEventListener('click', function () {
+  this.classList.toggle('l-header__humBtn--isOn'); //子要素の３本線をCSSアニメーション。
+  document.querySelector('.l-header__gnav').classList.toggle('l-header__gnav--show');
+});
+
+// いずれかのメニュー項目をクリックしたら、メニューを非表示にする。
+const elems_gnavItem = document.querySelectorAll('.l-header__gnav__item');
+if (elems_gnavItem.length > 0) {
+  elems_gnavItem.forEach(function (e) {
+    e.addEventListener('click', function () {
+      document.querySelector('.l-header__gnav').classList.remove('l-header__gnav--show');
+    });
+  });
+}
