e69dfba5b6e8c2f7334df8f36374f4cf5b961a49
taka <taka@users.noreply.github.com>
2024-11-18 11:53:53 +0900

    index.htmlのヘッダー部分のアニメーション実装。

--- js/script.js
+++ js/script.js
@@ -1,18 +1,26 @@
 'use strict';
 
 const swiper = new Swiper('.swiper', {
-    loop: true,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-  });
-  
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+});
+
+
+document.addEventListener('scroll', function () {
+  if (window.scrollY > 0) {
+    document.querySelector('.l-header').classList.add('l-header--hasScrolled');
+  } else {
+    document.querySelector('.l-header').classList.remove('l-header--hasScrolled');
+  }
+});
